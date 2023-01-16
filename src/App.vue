933ee6ffb1163f91b4f5c9ffd697000067c48db4
fedeloxe <loxeee@gmail.com>
2023-01-16 18:56:01 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import axios from 'axios';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+
+  }
+}
+
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+
+</template>
+
+<style lang="scss" scoped>
+@use './styles/generals.scss' as *;
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+
+}
+</script>
+<template>
+    <header>
+        <div class="logo">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/1/11/Yu-Gi-Oh%21_%28Logo%29.jpg" alt="">
+        </div>
+        <h1>Yu-Gi-Oh Api</h1>
+
+    </header>
+
+</template>
+
+
+<style lang="scss" scoped>
+header {
+    display: flex;
+}
+
+.logo img {
+    width: 200px;
+}
+</style>
